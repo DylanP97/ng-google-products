cfc0810fe71fa911c1f62113f518dd61c05acbc3
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-03-29 15:30:03 +0200

    chronology slider by year

--- src/app/chronology-slider/chronology-slider.component.ts
+++ src/app/chronology-slider/chronology-slider.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Observable } from 'rxjs';
 import { Product } from '../models/Product.model';
 import { DarkModeService } from '../services/dark-mode.service';
+import { MatSlider } from '@angular/material/slider';
 
 @Component({
   selector: 'app-chronology-slider',
   templateUrl: './chronology-slider.component.html',
   styleUrls: ['./chronology-slider.component.css']
 })
+
 export class ChronologySliderComponent implements OnInit {
   allProducts$!: Observable<Product[]>;
   products: any[] = [];
   loading!: boolean;
-  sliderPosition = 0;
-  sliderWidth = 0;
-  thumbWidth = 20; // add thumb width
-  isDragging = false;
-  currentX!: number;
+  startYear!: number;
+  endYear!: number;
+  selectedYear!: string;
 
   @ViewChild('slider') slider!: ElementRef;
 
@@ -35,64 +35,23 @@ export class ChronologySliderComponent implements OnInit {
         const bYear = new Date(b.yearLaunched).getFullYear();
         return aYear - bYear;
       });
+      this.startYear = new Date(this.products[0].yearLaunched).getFullYear();
+      this.endYear = new Date(this.products[this.products.length - 1].yearLaunched).getFullYear();
+      this.selectedYear = `${this.startYear}`;
       this.loading = false;
     });
+  } 
+  
+  onSliderChange(event: any) {
+    this.selectedYear = `${Number(event.target.value)}`;
   }
 
-  ngAfterViewInit(): void {
-    this.sliderWidth = this.slider.nativeElement.offsetWidth - this.thumbWidth; // subtract thumb width
-  }
 
-  getSliderPosition(dateString: string): number {
-    const date = new Date(dateString);
+  formatDBDateToYear(dateInDB: string): string {
+    const date = new Date(dateInDB);
     const year = date.getFullYear();
-    const startDate = this.products[0].yearLaunched;
-    const startNewDate = new Date(startDate)
-    const startYear = startNewDate.getFullYear()
-    const endDate = this.products[this.products.length - 1].yearLaunched;
-    const endNewDate = new Date(endDate)
-    const endYear = endNewDate.getFullYear()
-
-    console.log("year : " + year)
-
-    const maxSliderPosition = 100
-
-    const position = (maxSliderPosition * (year - startYear)) / (endYear - startYear)
-    console.log("position : " + position)
-
-    return position;
-  }
-  
-  
-  getTimestamp(dateString: string): number {
-    const date = new Date(dateString);
-    return date.getTime();
+    return `${year}`;
   }
 
-  // onMouseDown(event: MouseEvent) {
-  //   event.preventDefault();
-  //   this.isDragging = true;
-  //   this.currentX = event.clientX;
-  //   this.slider.nativeElement.classList.add('active');
-  //   this.slider.nativeElement.addEventListener('mousemove', this.onMouseMove.bind(this));
-  //   this.slider.nativeElement.addEventListener('mouseup', this.onMouseUp.bind(this));
-  // }
-  
-  // onMouseMove(event: MouseEvent) {
-  //   if (this.isDragging) {
-  //     const deltaX = event.clientX - this.currentX;
-  //     this.sliderPosition += deltaX / this.slider.nativeElement.offsetWidth * 100;
-  //     this.sliderPosition = Math.min(Math.max(this.sliderPosition, 0), 100);
-  //     this.currentX = event.clientX;
-  //   }
-  // }
-  
-  // onMouseUp() {
-  //   this.isDragging = false;
-  //   this.slider.nativeElement.classList.remove('active');
-  //   this.slider.nativeElement.removeEventListener('mousemove', this.onMouseMove.bind(this));
-  //   this.slider.nativeElement.removeEventListener('mouseup', this.onMouseUp.bind(this));
-  // }
-  
 
 }
